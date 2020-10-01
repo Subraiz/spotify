1f7757a5784a7a0490122c65cf4d3cbae0a74c96
Subraiz <subraizahmed@gmail.com>
2020-10-01 15:59:30 -0400

    added ability to create a playlist

--- apps/api/src/api/routes/playlist/PlaylistRouter.js
+++ apps/api/src/api/routes/playlist/PlaylistRouter.js
@@ -1,4 +1,67 @@
 const PlaylistRouter = require('express').Router();
 const axios = require('axios');
 
+PlaylistRouter.post('/create', async (req, res) => {
+  const {
+    user_id,
+    access_token,
+    name,
+    public,
+    collaborative,
+    description,
+  } = req.body;
+  const data = {
+    name,
+    description,
+    public: public || false,
+    collaborative: collaborative || false,
+  };
+  const url = `https://api.spotify.com/v1/users/${user_id}/playlists`;
+
+  const playlists = await getPlaylists(access_token);
+  let playlistExists = playlists.filter(function (playlist) {
+    return playlist.name === name;
+  })[0];
+  playlistExists = playlistExists == undefined ? false : true;
+
+  if (!playlistExists) {
+    await axios({
+      method: 'POST',
+      url,
+      data: data,
+      headers: {
+        Authorization: 'Bearer ' + access_token,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        res.status(200).send({ message: `Playlist ${name} has been created` });
+      })
+      .catch((err) => {
+        res.status(400).send({ message: 'Failed to create playlist' });
+      });
+  } else {
+    res.status(400).send({ message: 'Playlist already exists.' });
+  }
+});
+
+const getPlaylists = async (access_token) => {
+  const limit = 50;
+  const url = 'https://api.spotify.com/v1/me/playlists';
+
+  let playlist = [];
+
+  await axios({
+    method: 'GET',
+    url,
+    params: { limit },
+    headers: { Authorization: 'Bearer ' + access_token },
+  }).then((res) => {
+    playlists = res.data.items;
+    return playlists;
+  });
+
+  return playlists;
+};
+
 module.exports = PlaylistRouter;
--- apps/api/src/api/routes/user/UserRouter.js
+++ apps/api/src/api/routes/user/UserRouter.js
@@ -2,7 +2,9 @@ const UserRouter = require('express').Router();
 const axios = require('axios');
 
 UserRouter.get('/', async (req, res) => {
-  const access_token = req.query.access_token;
+  let access_token = req.query.access_token;
+  access_token =
+    'BQBbb4F-TFfp_DU_qk8C7TkAcytiKwmVf4gQl6R6IV0UgjxCP51VhXu-nqcwH0_iiRbYey2se0FuC4RHAxHPTZ1Dvg1UyPhm-xxO-oZTwLeoe24G9ehC6pF6xdq8IZ-rFovarWYUXjEbBb-ryR-KRNsAjreGKrXyUFggvcL_ljoMpBgDeVwoRZodriyyotTuFDk7LW1h_HSEBc1aP8KtVSHn-b_e8aoli4Qjcj6erX-RjA';
 
   const url = 'https://api.spotify.com/v1/me';
   await axios({
